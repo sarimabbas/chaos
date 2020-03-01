47d5c70f897cee916bb724ca0b9b85ce305d6927
Sarim Abbas <sarim.stays@gmail.com>
2020-03-01 14:35:15 -0500

    improve api and expose to modules as $chaos

--- src/frontend/src/api.js
+++ src/frontend/src/api.js
@@ -1,10 +1,45 @@
 import axios from "axios";
+import store from "./store";
+import events from "./events";
 
-const api = axios.create({
+const rest = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:5000/api"
       : "/api"
 });
+
+const refreshExplorer = () => {
+  store.dispatch("refreshFileExplorer");
+};
+
+const readFile = async path => {
+  return rest.get("/read-file", {
+    params: {
+      path: path
+    }
+  });
+};
+
+const writeFile = async (path, contents) => {
+  const response = await rest.post("/write-file", {
+    path: path,
+    data: contents
+  });
+  refreshExplorer();
+  return response;
+};
+
+const renameFile = () => {};
+
+const api = {
+  rest,
+  file: {
+    read: readFile,
+    write: writeFile,
+    rename: renameFile
+  },
+  refreshExplorer
+};
 
 export default api;
--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -2,14 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import events from "./events";
 
-import Icon from "vue-awesome/components/Icon";
+import api from "./api";
+
+// global component imports
+import VModal from "vue-js-modal";
+Vue.use(VModal);
 
 Vue.config.productionTip = false;
+// Vue.prototype.$backend = window.pywebview;
 
-Vue.prototype.$backend = window.pywebview;
-
-Vue.component("v-icon", Icon);
+Vue.prototype.$store = store;
+Vue.prototype.$events = events;
+Vue.prototype.$chaos = api;
 
 new Vue({
   router,
