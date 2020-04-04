219c0050006fc9780e27b3bdcf11e4a5b140ef25
Sarim Abbas <sarim.stays@gmail.com>
2020-04-04 12:12:39 -0400

    chat helo text

--- src/components/Contexts/Chat.vue
+++ src/components/Contexts/Chat.vue
@@ -3,21 +3,18 @@ export default {};
 </script>
 
 <template>
-  <div class="h-32">
-    <div class="mt-1 text-center ui-help-text">Chat Context</div>
-    <div class="mx-2 mt-1 mb-2">
-      <input
-        class="w-full px-2 py-1 text-xs placeholder-gray-700 bg-white border border-gray-300 rounded-sm appearance-none focus:outline-none focus:shadow-outline"
-        type="email"
-        placeholder="Search for a file or folder..."
-      />
+  <div class="flex items-center flex-none h-full">
+    <!-- help text -->
+    <div class="text-center">
+      <div class="px-16 py-4 mt-1 ui-help-text">
+        No Slack configuration found in your Explorer root. Would you like to
+        create one?
+      </div>
+      <button class="px-2 py-1 mr-2 text-base ui-button">
+        Create Slack configuration
+      </button>
     </div>
-
-    <div class="flex items-center justify-between px-1 py-1 bg-gray-800">
-      <span class="text-xs font-bold tracking-widest text-gray-400 uppercase">
-        Results
-      </span>
-    </div>
+    <!-- help end -->
   </div>
 </template>
 
