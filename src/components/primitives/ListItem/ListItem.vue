b0334341e334465c864ba1dcb89ff45273ac192e
Sarim Abbas <sarim.stays@gmail.com>
2020-04-08 22:02:53 -0400

    add quicklook to list items

--- src/components/primitives/ListItem/ListItem.vue
+++ src/components/primitives/ListItem/ListItem.vue
@@ -1,15 +1,35 @@
 <script>
+import EyeIcon from "../../../assets/icons/eye.svg";
+import { remote } from "electron";
+const currWindow = remote.getCurrentWindow();
 export default {
+  components: {
+    EyeIcon,
+  },
   props: ["click", "node", "title", "description", "icon"],
+  methods: {
+    quickLook(path) {
+      currWindow.previewFile(path);
+    },
+  },
 };
 </script>
 
 <template>
   <li
-    @click="click(node)"
-    class="flex items-center px-2 py-1 my-1 truncate bg-white border-gray-600 rounded-sm cursor-pointer"
+    class="flex items-center justify-between px-2 py-1 my-1 truncate bg-white border-gray-600 rounded-sm "
   >
-    <img :src="icon" /><span class="ml-2">{{ node.name }}</span>
+    <div class="flex items-center cursor-pointer" @click="click(node)">
+      <img :src="icon" class="h-8" />
+      <span class="ml-2">{{ node.name }}</span>
+    </div>
+    <div class="flex items-center">
+      <EyeIcon
+        width="20"
+        @click="quickLook(node.path)"
+        class="cursor-pointer"
+      />
+    </div>
   </li>
 </template>
 
